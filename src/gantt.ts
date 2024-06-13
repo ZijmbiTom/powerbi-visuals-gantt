9d5e75c62c113f8cc41f7762425846d83d3019cb
ZijmbiTom <155454273+ZijmbiTom@users.noreply.github.com>
2024-06-13 15:57:22 +0200

    Update gantt.ts

--- src/gantt.ts
+++ src/gantt.ts
@@ -274,7 +274,7 @@ export class Gantt implements IVisual {
 
     public static DefaultValues = {
         AxisTickSize: 6,
-        BarMargin: 0,
+        BarMargin: 5,
         ResourceWidth: 100,
         TaskColor: "#00B099",
         TaskLineColor: "#ccc",
